a9ce4b99d18411de59056a00c3d8dd5f3f118bba
max4-dev <maksim.bogomyakov@gmail.com>
2025-12-12 13:17:38 +0300

    added reset css

--- frontend/src/app/providers/RootProvider/RootProvider.tsx
+++ frontend/src/app/providers/RootProvider/RootProvider.tsx
@@ -1,6 +1,8 @@
 import { QueryProvider } from "../QueryProvider/QueryProvider"
 import { RouterProvider } from "../RouterProvider/RouterProvider"
 
+import "@/app/styles/style.css"
+
 export const RootProvider = () => {
   return (
     <QueryProvider>
